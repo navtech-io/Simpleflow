718d160e025fcabfa68835442c9e235045d7a33a
Rajesh Jinaga <rajesh.j@navaratan.com>
2022-07-29 14:46:06 +0530

    Update package configuration

--- build/Build.cs
+++ build/Build.cs
@@ -126,8 +126,8 @@ class Build : NukeBuild
                 .AddProperty("PackageIcon", @"PackageIcon.png")
                 .SetPackageRequireLicenseAcceptance(true)
                 .SetIncludeSymbols(true)
-                .SetDescription("Build dynamic rules and workflows using script")
-                .SetPackageTags("Simpleflow.NET Workflow RuleEngine DynamicExpressionEvaluator")
+                .SetDescription("A .NET library and a runtime engine to build dynamic rules and workflows with intuitive and simple Simpleflow scripting language.")
+                .SetPackageTags("Simpleflow.NET Workflow RuleEngine DynamicValidations DynamicExpressionEvaluator")
                 .SetNoDependencies(true)
                 .SetOutputDirectory(ArtifactsDirectory / "nuget"));
         });
